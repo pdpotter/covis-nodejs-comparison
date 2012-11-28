6795e1128029d627ada66b67d5632185991e7499
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2012-11-28 17:11:04 +0100

    working version with node-pngjs

--- speedcomp.js
+++ speedcomp.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
-var fs   = require('fs'),
-    exec = require('child_process').exec;
-    png  = require('png-js');
-    argv = require('optimist').usage('Usage: $0 -i inputfolder -oc coutputfolder -oj joutputfolder')
-                              .demand(['i','oc','oj'])
+var fs      = require('fs'),
+    request = require('request'),
+    argv    = require('optimist').usage('Usage: $0 -i inputfolder')
+                              .demand(['i'])
                               .argv;
 
 var start;
@@ -13,8 +12,10 @@ var inputfiles;
 var outputfiles;
 var running = 0,
     limit   = 200;
+var urls = [];
 
-var steps = [initc,removecoutput,initjs,removejsoutput,initc,removecoutput,initjs,removejsoutput];
+var steps = [uploadc, grayc, emptyurls, uploadjs, grayjs, emptyurls, uploadc, grayc, emptyurls, uploadjs, grayjs, emptyurls];
+//var steps = [uploadjs, grayjs, emptyurls];
 executeSteps();
 
 function executeSteps() {
@@ -28,35 +29,44 @@ function executeSteps() {
   });
 }
 
-function initc(callback){
-  console.log('Executing c code externally');
-  execstep(singlec, argv.i, argv.oc, fs.readdirSync(argv.i), new Date(), function(){callback();});
+function uploadc(callback){
+  console.log('Uploading for wrapped c++ code');
+  execstep(uploadsinglec, argv.i, fs.readdirSync(argv.i), new Date(), function(){callback();});
 }
 
-function initjs(callback){
-  console.log('Executing wrapped code');
-  execstep(singlejs, argv.i, argv.oj, fs.readdirSync(argv.i), new Date(), function(){callback();});
+function grayc(callback){
+  console.log('Executing wrapped c++ code');
+  execstep(graysinglec, argv.i, fs.readdirSync(argv.i), new Date(), function(){callback();});
 }
 
-function removecoutput(callback){
-  emptydir(argv.oc, fs.readdirSync(argv.oc), function(){callback();});
+function emptyurls(callback){
+  console.log('Clearing url list');
+  urls.length = 0;
+  callback();
 }
 
-function removejsoutput(callback){
-  emptydir(argv.oj, fs.readdirSync(argv.oj), function(){callback();});
+function uploadjs(callback){
+  console.log('Uploading for native js code (pngjs)');
+  execstep(uploadsinglejs, argv.i, fs.readdirSync(argv.i), new Date(), function(){callback();});
 }
 
-function execstep(func, pathin, pathout, files, start, callback){
+function grayjs(callback){
+  console.log('Executing native js code (pngjs)');
+  execstep(graysinglejs, argv.i, fs.readdirSync(argv.i), new Date(), function(){callback();});
+}
+
+function execstep(func, pathin, files, start, callback){
   while(running < limit && files.length > 0) {
     running++;
     //console.log(running);
     var file = files.shift();
-    func(pathin, pathout, file, function(err) {
+    func(pathin, file, function(err) {
       if (err)
         throw err;
       running--;
+      //console.log(running);
       if(files.length > 0) {
-        execstep(func, pathin, pathout, files, start, callback);
+        execstep(func, pathin, files, start, callback);
       }
       else if(running == 0){
         duration = new Date() - start;
@@ -67,34 +77,42 @@ function execstep(func, pathin, pathout, files, start, callback){
   }
 }
 
-function singlec(pathin, pathout, file, callback) {
-  exec('./Gray -i ' + pathin + '/' + file + ' -o ' + pathout + '/' + file, function(err, stdout, stderr){
+function uploadsinglec(pathin, file, callback) {
+  var r = request.post({url:'http://localhost:8802/images', headers:''}, function(err, resp, body) {
+    if (err)
+      throw err;
+    urls.push(resp.headers.location);
+    callback();
+  });
+  var form = r.form();
+  form.append('image', fs.ReadStream(pathin + '/' + file));
+}
+
+function graysinglec(pathin, file, callback) {
+  var index = file.split('.')[0];
+  request.get('http://localhost:8802/grays/' + urls[index].split('/')[2], function(err, resp, body) {
     if (err)
       throw err;
     callback();
   });
 }
 
-function singlejs(pathin, pathout, file, callback) {
-  png.decode(pathin + '/' + file, function(pixels) {
+function uploadsinglejs(pathin, file, callback) {
+  var r = request.post({url:'http://localhost:8812/images', headers:''}, function(err, resp, body) {
+    if (err)
+      throw err;
+    urls.push(resp.headers.location);
     callback();
   });
+  var form = r.form();
+  form.append('image', fs.ReadStream(pathin + '/' + file));
 }
 
-function emptydir(path, files, callback) {
-  while(running < limit && files.length > 0) {
-    var file = files.shift();
-    fs.unlink(path + '/' + file, function (err) {
-      if (err)
-        throw err;
-      running--;
-      if(files.length > 0) {
-        emptydir(path, files, callback);
-      }
-      else if(running == 0){
-        callback();
-      }
-    });
-    running++;
-  }
+function graysinglejs(pathin, file, callback) {
+  var index = file.split('.')[0];
+  request.get('http://localhost:8812/grays/' + urls[index].split('/')[2], function(err, resp, body) {
+    if (err)
+      throw err;
+    callback();
+  });
 }
