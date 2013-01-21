3cb3b3a57425438531b129b534704d2ae0a3090d
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-01-21 16:58:57 +0100

    Added bindings gaussian blur + jshint

--- servers/bindings.js
+++ servers/bindings.js
@@ -4,14 +4,15 @@
           Filter server
 ***/
 
-//require('nodetime').profile();
-
 var express = require('express'),
     cluster = require('cluster'),
     fs = require('fs'),
     cv = require('../../opencv-node'),
     serverutils = require('../common/descr/server-util.js'),
     images = require('../common/descr/image-library.js').Images;
+
+var getGray,
+    getGauss;
 
 if (cluster.isMaster) {
   require('os').cpus().forEach(function() {
@@ -35,32 +36,44 @@ if (cluster.isMaster) {
   app.listen(port);
   console.log('onlyjs server running on http://' + host + ':' + port);
 
-  /***    routing    ***/
-
-  app.get(/^\/grays\/(\d+)$/, getGray);
-
   /***    handlers    ***/
 
-  function getGray(req, res, next) {
+  getGray = function(req, res, next) {
     var img = images.get(req.params[0]);
     if (img) {
-      var resultname = '../tmp/' + 'grays_' + img.id + '.' + img.extension;
       // read png
       var src = cv.imread(img.fileName);
-      var gray = new cv.Mat;
+      var gray = new cv.Mat();
       cv.cvtColor(src, gray, cv.CV_BGR2GRAY);
-      //var buf = new Buffer(cv.imencode('.png', gray),'binary');
       var buf = cv.imencode('.png', gray);
       res.header('Content-Type', images.exts_getconttype(img.extension));
       res.send(buf);
-      //cv.imwrite(resultname, gray);
-      //serverutils.respond.withFile(res, resultname, images.exts_getconttype(img.extension), null, function(err){
-      //  fs.unlink(resultname);
-      //});
     }
     else {
       res.header('Link','</grays>; rel="index"');
       res.send('', 404);
     }
-  }
+  };
+  
+  getGauss = function(req, res, next) {
+    var img = images.get(req.params[0]);
+    if (img) {
+      // read png
+      var src = cv.imread(img.fileName);
+      var gauss = new cv.Mat(src.size, src.type);
+      cv.GaussianBlur(src, gauss, {width: 7, height: 7}, 0);
+      var buf = cv.imencode('.png', gauss);
+      res.header('Content-Type', images.exts_getconttype(img.extension));
+      res.send(buf);
+    }
+    else {
+      res.header('Link','</grays>; rel="index"');
+      res.send('', 404);
+    }
+  };
+
+  /***    routing    ***/
+
+  app.get(/^\/grays\/(\d+)$/, getGray);
+  app.get(/^\/gauss\/(\d+)$/, getGauss);
 }
