ffd2556e1f8fb047896e4f98c1d084a712a72888
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-02-06 17:29:35 +0100

    cwrapper read from url gray

--- servers/cwrapper.js
+++ servers/cwrapper.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 /***
-          Filter server
+          External program call server
 ***/
-
 
 var express = require('express'),
     cluster = require('cluster'),
+    http = require('http'),
     fs = require('fs'),
     exec = require('child_process').exec,
     serverutils = require('../common/descr/server-util.js'),
@@ -25,7 +25,7 @@ if (cluster.isMaster) {
 
   var app = exports.server = express(),
       port = process.env.PORT || 9002,
-      host = process.env.PORT || '127.0.0.1';
+      host = process.env.HOST || '127.0.0.1';
 
   /***    configure server    ***/
 
@@ -35,25 +35,30 @@ if (cluster.isMaster) {
   /***    start server    ***/
 
   app.listen(port);
-  console.log('cwrapper server running on http://' + host + ':' + port);
+  console.log('External program call server running on http://' + host + ':' + port);
 
   /***    handlers    ***/
 
   getGray = function(req, res, next) {
-    var img = images.get(req.params[0]);
-    if (img) {
-      var resultname = '../tmp/' + 'grays_' + img.id + '.' + img.extension;
-      exec('../alg/Gray/Gray -i ' + img.fileName + ' -o ' + resultname, function(err, stdout, stderr){
-        if (err) throw err;
-        serverutils.respond.withFile(res, resultname, images.exts_getconttype(img.extension), null, function(err){
-          fs.unlink(resultname);
-        });
-      });
-    }
-    else {
-      res.header('Link','</grays>; rel="index"');
-      res.send('', 404);
-    }
+    var img = req.params[0];
+    var filename = '../tmp/' + img + '.png';
+    var resultname = '../tmp/' + 'grays_' + img + '.png';
+    var writestream = fs.createWriteStream(filename);
+    http.get('http://127.0.0.1:9000/' + img + '.png', function(serverres) {
+      serverres.pipe(writestream);
+      serverres.on('end', function(){
+                 exec('../alg/Gray/Gray -i ' + filename + ' -o ' + resultname, function(err, stdout, stderr){
+                   if (err) throw err;
+                   fs.readFile(resultname, function (err, data) {
+                     if (err) throw err;
+                     res.header('Content-Type', 'image/png');
+                     res.send(data);
+                     fs.unlink(filename);
+                     fs.unlink(resultname);
+                   });
+                 });
+               });
+    });
   };
   
   getGauss = function(req, res, next) {
