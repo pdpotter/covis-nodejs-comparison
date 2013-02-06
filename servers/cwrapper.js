1cae9b789699c5c12b320e124b321477913f2ad8
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-02-06 17:36:20 +0100

    read from url complete and cleanup

--- servers/cwrapper.js
+++ servers/cwrapper.js
@@ -8,9 +8,7 @@ var express = require('express'),
     cluster = require('cluster'),
     http = require('http'),
     fs = require('fs'),
-    exec = require('child_process').exec,
-    serverutils = require('../common/descr/server-util.js'),
-    images = require('../common/descr/image-library.js').Images;
+    exec = require('child_process').exec;
 
 var getGray,
     getGauss;
@@ -62,20 +60,25 @@ if (cluster.isMaster) {
   };
   
   getGauss = function(req, res, next) {
-    var img = images.get(req.params[0]);
-    if (img) {
-      var resultname = '../tmp/' + 'grays_' + img.id + '.' + img.extension;
-      exec('../alg/Gaussian/Gaussian -i ' + img.fileName + ' -o ' + resultname, function(err, stdout, stderr){
-        if (err) throw err;
-        serverutils.respond.withFile(res, resultname, images.exts_getconttype(img.extension), null, function(err){
-          fs.unlink(resultname);
-        });
-      });
-    }
-    else {
-      res.header('Link','</gauss>; rel="index"');
-      res.send('', 404);
-    }
+    var img = req.params[0];
+    var filename = '../tmp/' + img + '.png';
+    var resultname = '../tmp/' + 'grays_' + img + '.png';
+    var writestream = fs.createWriteStream(filename);
+    http.get('http://127.0.0.1:9000/' + img + '.png', function(serverres) {
+      serverres.pipe(writestream);
+      serverres.on('end', function(){
+                 exec('../alg/Gaussian/Gaussian -i ' + filename + ' -o ' + resultname, function(err, stdout, stderr){
+                   if (err) throw err;
+                   fs.readFile(resultname, function (err, data) {
+                     if (err) throw err;
+                     res.header('Content-Type', 'image/png');
+                     res.send(data);
+                     fs.unlink(filename);
+                     fs.unlink(resultname);
+                   });
+                 });
+               });
+    });
   };
   
   /***    routing    ***/
