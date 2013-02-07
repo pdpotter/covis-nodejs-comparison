3c90ff05fd406de87fe66c1793b99d6d2d281bf1
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-02-07 17:50:57 +0100

    memusage improvements

--- memusage.sh
+++ memusage.sh
@@ -4,11 +4,8 @@ cd servers
 ./images.js &
 sleep 1
 ./onlyjs.js &
+#./cwrapper.js &
 sleep 1
-pid1=`ps ax | grep nodejs | sed -n 1p | cut -d' ' -f1`
-pid2=`ps ax | grep nodejs | sed -n 2p | cut -d' ' -f1`
-pid3=`ps ax | grep nodejs | sed -n 3p | cut -d' ' -f1`
-pid4=`ps ax | grep nodejs | sed -n 4p | cut -d' ' -f1`
 
 cd ..
 sleep 1
@@ -18,10 +15,9 @@ speedcompid=$!
 rm mem.txt
 
 while true; do
-  pmap -x $pid1 | grep "total" | tr -s ' ' | cut -d' ' -f4 >> "mem.txt"
-  pmap -x $pid2 | grep "total" | tr -s ' ' | cut -d' ' -f4 >> "mem.txt"
-  pmap -x $pid3 | grep "total" | tr -s ' ' | cut -d' ' -f4 >> "mem.txt"
-  pmap -x $pid4 | grep "total" | tr -s ' ' | cut -d' ' -f4 >> "mem.txt"
+  pmap -x $(pidof nodejs) | grep total | tr -s ' ' | cut -d' ' -f4 >> "mem.txt"
+  pmap -x $(pidof Gray) | grep total | tr -s ' ' | cut -d' ' -f4 2> /dev/null
+  echo "" >> "mem.txt" &
   sleep 1
   if ! [ $(ps -o pid= -p $speedcompid) ]
   then
