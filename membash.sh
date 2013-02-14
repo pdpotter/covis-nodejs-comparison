9e6f8f1521295c1f312206ab345a45e8ea741b52
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-02-14 18:58:35 +0100

    renaming in membash

--- membash.sh
+++ membash.sh
@@ -6,7 +6,7 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, grayjs/g' speedcomp.js
-sed -i 's/commandplease/onlyjs.js/g' memusage.sh
+sed -i 's/commandplease/pure_javascript.js/g' memusage.sh
 
 ./memusage.sh >> results/grayjs200.txt
 
@@ -14,7 +14,7 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, grayc/g' speedcomp.js
-sed -i 's/commandplease/cwrapper.js/g' memusage.sh
+sed -i 's/commandplease/external_program_call.js/g' memusage.sh
 
 ./memusage.sh >> results/grayc200.txt
 
@@ -22,7 +22,7 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, grayb/g' speedcomp.js
-sed -i 's/commandplease/bindings.js/g' memusage.sh
+sed -i 's/commandplease/native_add-on.js/g' memusage.sh
 
 ./memusage.sh >> results/grayb200.txt
 
@@ -30,7 +30,7 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, gaussjs/g' speedcomp.js
-sed -i 's/commandplease/onlyjs.js/g' memusage.sh
+sed -i 's/commandplease/pure_javascript.js/g' memusage.sh
 
 ./memusage.sh >> results/gaussjs200.txt
 
@@ -38,7 +38,7 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, gaussc/g' speedcomp.js
-sed -i 's/commandplease/cwrapper.js/g' memusage.sh
+sed -i 's/commandplease/external_program_call.js/g' memusage.sh
 
 ./memusage.sh >> results/gaussc200.txt
 
@@ -46,7 +46,7 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, gaussb/g' speedcomp.js
-sed -i 's/commandplease/bindings.js/g' memusage.sh
+sed -i 's/commandplease/native_add-on.js/g' memusage.sh
 
 ./memusage.sh >> results/gaussb200.txt
 
@@ -57,7 +57,7 @@ cp speedcompclean.js speedcomp.js
 
 sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
 sed -i 's/commandsplease/createlist, grayjs/g' speedcomp.js
-sed -i 's/commandplease/onlyjs.js/g' memusage.sh
+sed -i 's/commandplease/pure_javascript.js/g' memusage.sh
 
 ./memusage.sh >> results/grayjs1.txt
 
@@ -66,7 +66,7 @@ cp speedcompclean.js speedcomp.js
 
 sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
 sed -i 's/commandsplease/createlist, grayc/g' speedcomp.js
-sed -i 's/commandplease/cwrapper.js/g' memusage.sh
+sed -i 's/commandplease/external_program_call.js/g' memusage.sh
 
 ./memusage.sh >> results/grayc1.txt
 
@@ -75,7 +75,7 @@ cp speedcompclean.js speedcomp.js
 
 sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
 sed -i 's/commandsplease/createlist, grayb/g' speedcomp.js
-sed -i 's/commandplease/bindings.js/g' memusage.sh
+sed -i 's/commandplease/native_add-on.js/g' memusage.sh
 
 ./memusage.sh >> results/grayb1.txt
 
@@ -84,7 +84,7 @@ cp speedcompclean.js speedcomp.js
 
 sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
 sed -i 's/commandsplease/createlist, gaussjs/g' speedcomp.js
-sed -i 's/commandplease/onlyjs.js/g' memusage.sh
+sed -i 's/commandplease/pure_javascript.js/g' memusage.sh
 
 ./memusage.sh >> results/gaussjs1.txt
 
@@ -93,7 +93,7 @@ cp speedcompclean.js speedcomp.js
 
 sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
 sed -i 's/commandsplease/createlist, gaussc/g' speedcomp.js
-sed -i 's/commandplease/cwrapper.js/g' memusage.sh
+sed -i 's/commandplease/external_program_call.js/g' memusage.sh
 
 ./memusage.sh >> results/gaussc1.txt
 
@@ -102,7 +102,7 @@ cp speedcompclean.js speedcomp.js
 
 sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
 sed -i 's/commandsplease/createlist, gaussb/g' speedcomp.js
-sed -i 's/commandplease/bindings.js/g' memusage.sh
+sed -i 's/commandplease/native_add-on.js/g' memusage.sh
 
 ./memusage.sh >> results/gaussb1.txt
 
@@ -114,34 +114,22 @@ cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
 sed -i 's/commandsplease/createlist, grayb/g' speedcomp.js
-sed -i 's/.\/commandplease/node --expose_gc bindings_mem.js/g' memusage.sh
+sed -i 's/.\/commandplease/node --expose_gc native_add-on.js gc/g' memusage.sh
 
 ./memusage.sh >> results/gc_grayb200.txt
 
 cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
-sed -i 's/commandsplease/createlist, gaussb/g' speedcomp.js
-sed -i 's/.\/commandplease/node --expose_gc bindings_mem.js/g' memusage.sh
+sed -i 's/commandsplease/createlist, grayjs/g' speedcomp.js
+sed -i 's/.\/commandplease/node --expose_gc pure_javascript.js gc/g' memusage.sh
 
-./memusage.sh >> results/gc_gaussb200.txt
-
-# limit = 1
+./memusage.sh >> results/gc_grayjs200.txt
 
 cp memusageclean.sh memusage.sh
 cp speedcompclean.js speedcomp.js
 
-sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
-sed -i 's/commandsplease/createlist, grayb/g' speedcomp.js
-sed -i 's/.\/commandplease/node --expose_gc bindings_mem.js/g' memusage.sh
+sed -i 's/commandsplease/createlist, grayc/g' speedcomp.js
+sed -i 's/.\/commandplease/node --expose_gc external_program_call.js gc/g' memusage.sh
 
-./memusage.sh >> results/gc_grayb1.txt
-
-cp memusageclean.sh memusage.sh
-cp speedcompclean.js speedcomp.js
-
-sed -i 's/limit   = 200/limit   = 1/g' speedcomp.js
-sed -i 's/commandsplease/createlist, gaussb/g' speedcomp.js
-sed -i 's/.\/commandplease/node --expose_gc bindings_mem.js/g' memusage.sh
-
-./memusage.sh >> results/gc_gaussb1.txt
+./memusage.sh >> results/gc_grayc200.txt
