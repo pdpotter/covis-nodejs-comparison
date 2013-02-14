7d51b4199d74613d11af6e92382c69350e8873de
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-02-14 18:25:39 +0100

    added onlyjs comments

--- servers/onlyjs.js
+++ servers/onlyjs.js
@@ -4,6 +4,38 @@
           Pure JavaScript server
 ***/
 
+/***
+
+  Various implementations exist for PNG de/encoding in Node.js:
+  * node-pngjs (https://github.com/niegowski/node-pngjs)
+  is the only Node.js package that provides both encoding and decoding using only Javascript.
+  * png.js (https://github.com/devongovett/png.js) only does PNG decoding.
+  * node-png  (https://github.com/pkrumins/node-png) uses the native library libpng.
+
+  Various implementations exist for conversion to gray scale and the Gaussian blur filter.
+  * Conversion to gray scale
+    - This can be implemented very fast by calculating
+      gray = 0.299 * red + 0.587 * green + 0.114 * blue
+      for each pixel value.
+    - However, the implementation in Jsfeat is faster by forcing integer operations.
+  
+  * Gaussian blur filter
+    - The Kraken benchmark (http://krakenbenchmark.mozilla.org/explanations/gaussian-blur.html)
+      This implementation is relatively slow compared to the others.
+    - StackBlur (http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html)
+      This is the fastest implementation, but is actually a mix between Gaussian blur and Box blur
+    - A JavaScript implementation of
+      Ian T. Young , Lucas J. van Vliet. Recursive implementation of the Gaussian filter.
+      Signal processing, vol. 44, no. 1, pp. 139-151, 1995.
+      as proposed by Martin (http://www.blogger.com/profile/02959903552471306591)
+      in a reaction on a blogpost by Paul Nickerson
+      (http://pvnick.blogspot.be/2010/01/im-currently-porting-image-segmentation.html)
+    - Jsfeat has the fastest pure Gaussian blur implementation.
+    
+  Jsfeat is provided as a library and is even available as a package for node.js (jsfeat)
+  
+***/
+
 var express = require('express'),
     cluster = require('cluster'),
     http = require('http'),
@@ -18,10 +50,6 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-
-  /***    helper functions    ***/
-
- 
 
   /***    create server    ***/
 
@@ -47,16 +75,21 @@ if (cluster.isMaster) {
     http.get('http://127.0.0.1:9000/' + img + '.png', function(serverres) {
       serverres.pipe(new PNG({deflateLevel: 1, filterType: [0,1]}))
                .on('parsed',function(){
+                  // convert to gray scale
                   var i,j;
                   var dst = new Buffer(this.data.length/4);
                   jsfeat.imgproc.grayscale(this.data, dst);
                   for (i = j = 0; i < dst.length; i++, j += 4) {
                     this.data[j] = this.data[j + 1] = this.data[j + 2] = dst[i];
                   }
-                  // write png
+                  // send result
                   res.header('Content-Type', 'image/png');
                   this.pack()
                       .pipe(res);
+                  // manual garbage collection if requested
+                  if (process.argv[2] === "gc") {
+                    global.gc();
+                  }
                 });
     });
   };
@@ -67,6 +100,7 @@ if (cluster.isMaster) {
     http.get('http://127.0.0.1:9000/' + img + '.png', function(serverres) {
       serverres.pipe(new PNG({deflateLevel: 1, filterType: [0,1]}))
                .on('parsed',function(){
+                  // Gaussian blur filter
                   var i,j;
                   var red = new jsfeat.matrix_t(this.width, this.height, jsfeat.U8_t | jsfeat.C1_t);
                   var green = new jsfeat.matrix_t(this.width, this.height, jsfeat.U8_t | jsfeat.C1_t);
@@ -87,10 +121,14 @@ if (cluster.isMaster) {
                     this.data[i + 1] = ggreen.data[j];
                     this.data[i + 2] = gblue.data[j];
                   }
-                  // write png
+                  // send result
                   res.header('Content-Type', 'image/png');
                   this.pack()
                       .pipe(res);
+                  // manual garbage collection if requested
+                  if (process.argv[2] === "gc") {
+                    global.gc();
+                  }
                 });
                  
     });
