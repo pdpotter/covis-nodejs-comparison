29e1576dddae97f10bcb6789527704907e59d1a7
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2013-02-14 17:43:06 +0100

    added bindings comments

--- servers/bindings.js
+++ servers/bindings.js
@@ -4,9 +4,26 @@
           Native add-on server
 ***/
 
+/***
+
+  Two existing project provide Node.js bindings for OpenCV:
+  https://github.com/peterbraden/node-opencv and https://github.com/codeboost/opencv-node
+  
+  
+  https://github.com/codeboost/opencv-node is used since it uses a semi-automatic approach,
+  using https://github.com/codeboost/bea, to expose C++ libraries to Javascript.
+  This makes it a lot easier to add additional functions to expose
+  and to maintain the native add-on in case of changes in the library.
+
+***/
+
 var express = require('express'),
     cluster = require('cluster'),
     http = require('http'),
+    // this is a slightly adapted version of opencv-node
+    // implementing imencode and imdecode using Node::Buffer
+    // te adapted version can be found on
+    // https://github.com/pdpotter/opencv-node/tree/using_buffer
     cv = require('../../opencv-node');
 
 var getGray,
@@ -47,12 +64,18 @@ if (cluster.isMaster) {
         offset = offset + chunk.length;
       });
       serverres.on('end', function(){
+        // convert to gray scale
         var src = new cv.Mat(cv.imdecode(rawdata,1));
         var gray = new cv.Mat();
         cv.cvtColor(src, gray, cv.CV_BGR2GRAY);
         var buf = cv.imencode('.png', gray);
+        // send result
         res.header('Content-Type', 'image/png');
         res.send(buf);
+        // manual garbage collection if wanted
+        if (process.argv[2] === "gc") {
+          global.gc();
+        }
       });
     });
   };
@@ -68,12 +91,18 @@ if (cluster.isMaster) {
         offset = offset + chunk.length;
       });
       serverres.on('end', function(){
+        // gaussian blur filter
         var src = new cv.Mat(cv.imdecode(rawdata,1));
         var gauss = new cv.Mat(src.size, src.type);
         cv.GaussianBlur(src, gauss, {width: 7, height: 7}, 0);
         var buf = cv.imencode('.png', gauss);
+        // send result
         res.header('Content-Type', 'image/png');
         res.send(buf);
+        // manual garbage collection if wanted
+        if (process.argv[2] === "gc") {
+          global.gc();
+        }
       });
     });
   };
